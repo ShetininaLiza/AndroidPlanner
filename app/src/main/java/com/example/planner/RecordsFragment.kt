9986907145ce32bbacb61bf542c3a7636a0bee9f
ShetininaLiza <shetinina.l2000@gmail.com>
2026-04-14 16:26:29 +0400

    Доработка списка

--- app/src/main/java/com/example/planner/RecordsFragment.kt
+++ app/src/main/java/com/example/planner/RecordsFragment.kt
@@ -7,6 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.planner.adapter.RecordAdapter
+import com.example.planner.model.RecordItemClass
 import java.io.FileDescriptor
 import java.io.PrintWriter
 
@@ -28,7 +33,21 @@ class RecordsFragment : Fragment(R.layout.fragment_records) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnAddRecord = view.findViewById<Button>(R.id.btnAddRecord)
+        val recordsRecycle = view.findViewById<RecyclerView>(R.id.recordsList)
+        val recordAdapter = RecordAdapter();
+        recordsRecycle.layoutManager = LinearLayoutManager(view.context)
+        var list: MutableList<RecordItemClass> = mutableListOf();
+        for(i in 1..5 step 1){
+            var data = RecordItemClass("Заметка $i")
+            list.add(data)
+        }
+        Log.v("FRAGMENT", list.size.toString())
+        recordAdapter.data= list;
+
+        recordsRecycle.adapter = recordAdapter
+
+        val btnAddRecord = view.findViewById<ImageButton>(R.id.btnAddRecord)
+
         btnAddRecord.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, AddRecordFragment())
---
+++ app/src/main/java/com/example/planner/model/RecordItemClass.kt
@@ -0,0 +1,6 @@
+package com.example.planner.model
+
+//Данные для элемента списка
+data class RecordItemClass (
+    val title : String
+)
