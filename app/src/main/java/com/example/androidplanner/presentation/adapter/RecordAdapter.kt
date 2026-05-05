a959ed76b8c8a5d26b2c577cbc369d8fc0e8c6da
ShetininaLiza <shetinina.l2000@gmail.com>
2026-05-05 12:56:11 +0400

    Добавление окна для подтверждения удаления заметки

--- app/src/main/java/com/example/androidplanner/presentation/adapter/RecordAdapter.kt
+++ app/src/main/java/com/example/androidplanner/presentation/adapter/RecordAdapter.kt
@@ -1,11 +1,18 @@
 package com.example.androidplanner.presentation.adapter
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.os.Message
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 import com.example.androidplanner.R
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidplanner.presentation.models.PresentationRecordItem
@@ -22,11 +29,24 @@ class RecordAdapter(private val context: Context) : RecyclerView.Adapter<RecordA
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
         Log.v("Adapter onCreateViewHolder", "____________onCreateViewHolder")
         val itemView = LayoutInflater.from(context)
-            //parent.context)
             .inflate(R.layout.record_item, parent, false)
+        val btnDell = itemView.findViewById<ImageButton>(R.id.btnDelRecord)
+        btnDell.setOnClickListener { delRecord() }
         return RecordViewHolder(itemView)
     }
-
+    fun delRecord(){
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Удаление")
+        //builder.setMessage("Вы хотите удалить " + phone +"?")
+        builder.setMessage("Вы хотите удалить?")
+        //крестик
+        //builder.setIcon(android.R.drawable.ic_delete)
+        //корзинка
+        builder.setIcon(android.R.drawable.ic_menu_delete)
+        builder.setPositiveButton("Нет", null)
+        builder.setNegativeButton("Да", null)
+        builder.show()
+    }
     override fun onBindViewHolder(
         holder: RecordViewHolder,
         pozition: Int
@@ -34,6 +54,7 @@ class RecordAdapter(private val context: Context) : RecyclerView.Adapter<RecordA
         // let — одна из функций области видимости (scope functions),
         // которая позволяет выполнить блок кода в контексте объекта.
         data[pozition].let {
+            holder.idRecord = data[pozition].id
             holder.titleRecord.setText(data[pozition].title)
             holder.textRecord.setText(data[pozition].text)
         }
@@ -41,7 +62,11 @@ class RecordAdapter(private val context: Context) : RecyclerView.Adapter<RecordA
 
     override fun getItemCount(): Int = data.size
     class RecordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //id заметки
+        var idRecord: Int = 0
+        //заголовок заметки
         val titleRecord = itemView.findViewById<TextView>(R.id.text_titleRecord)
+        //текст заметки
         val textRecord = itemView.findViewById<TextView>(R.id.textRecord)
     }
     /*
